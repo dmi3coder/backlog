ff72048b648b7be02b5d24b27ede60a084dbca57
dmi3coder <dmi3coder@gmail.com>
2016-07-22 15:31:20 +0300

    Added MainActivity

---
+++ core/src/com/dmi3coder/backlog/MainActivity.java
@@ -0,0 +1,28 @@
+package com.dmi3coder.backlog;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainActivity extends Game {
+    private SpriteBatch batch;
+    private BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
